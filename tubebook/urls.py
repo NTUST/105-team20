021acc154588fb54faafdc6b23cb7e9e0e410f9a
Miles <swrev5741@gmail.com>
2016-06-17 11:20:55 +0800

    add not found page

--- tubebook/urls.py
+++ tubebook/urls.py
@@ -5,11 +5,11 @@ from tubebook_main.views import *
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', firstindex),
-    url(r'^(\d)/$', index),
-    url(r'^post/(\d)/$', post),
+    url(r'^(\d{1,3})/$', index),
+    url(r'^post/(\d{1,3})/$', post),
     url(r'^about/$', about),
     url(r'^author/$', authorList),
-    url(r'^author/(\d)/$', authorProfile),
-    url(r'^tag/(\d)/(\d)/$', postListByTag),
-    url(r'^author/(\d)/(\d)/$', postListByAuthor)
+    url(r'^author/(\d{1,3})/$', authorProfile),
+    url(r'^tag/(\d{1,3})/(\d{1,3})/$', postListByTag),
+    url(r'^author/(\d{1,3})/(\d{1,3})/$', postListByAuthor)
 ]
--- tubebook_main/views.py
+++ tubebook_main/views.py
@@ -5,43 +5,90 @@ def firstindex(request):
     return index(request, 1)
 
 def index(request, page):
-    posts = PostList.getPostList(page)
-    sidePosts = PostList.getPostList(1)
-    page = PostList.getPageInfo(page)
-    tages = TagList.getTagList()
+    try:
+        posts = PostList.getPostList(page)
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page)
+        tages = TagList.getTagList()
 
-    return render_to_response('index.html', locals())
+        return render_to_response('index.html', locals())
+    except:
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page)
+        tages = TagList.getTagList()
+        return render_to_response('404.html', locals())
 
 def post(request, id):
-    post = PostList.getPost(id)
-    return render_to_response('post.html', locals())
+    try:
+        post = PostList.getPost(id)
+        return render_to_response('post.html', locals())
+    except:
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page)
+        tages = TagList.getTagList()
+        return render_to_response('404.html', locals())
+
 
 def about(request):
-    return render_to_response('about.html', locals())
+    try:
+        return render_to_response('about.html', locals())
+    except:
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page)
+        tages = TagList.getTagList()
+        return render_to_response('404.html', locals())
+
 
 def authorList(request):
-    authors = Authors.getAuthorList()
-    return render_to_response('author.html', locals())
+    try:
+        authors = Authors.getAuthorList()
+        return render_to_response('author.html', locals())
+    except:
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page)
+        tages = TagList.getTagList()
+        return render_to_response('404.html', locals())
+
 
 def authorProfile(request, id):
-    author = Authors.getAuthorProfile(id)
-    posts = PostList.getPostList(1, "author", id)
-    return render_to_response('author_profile.html', locals())
+    try:
+        author = Authors.getAuthorProfile(id)
+        posts = PostList.getPostList(1, "author", id)
+        return render_to_response('author_profile.html', locals())
+    except:
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page)
+        tages = TagList.getTagList()
+        return render_to_response('404.html', locals())
+
 
 def postListByTag(request, id, page):
-    posts = PostList.getPostList(page, "tag", id)
-    sidePosts = PostList.getPostList(1)
-    page = PostList.getPageInfo(page, "tag", id)
-    tages = TagList.getTagList()
-    tag = TagList.getTag(id)
+    try:
+        posts = PostList.getPostList(page, "tag", id)
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page, "tag", id)
+        tages = TagList.getTagList()
+        tag = TagList.getTag(id)
 
-    return render_to_response('tag_post.html', locals())
+        return render_to_response('tag_post.html', locals())
+    except:
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page)
+        tages = TagList.getTagList()
+        return render_to_response('404.html', locals())
+
 
 def postListByAuthor(request, id, page):
-    posts = PostList.getPostList(page, "author", id)
-    sidePosts = PostList.getPostList(1)
-    page = PostList.getPageInfo(page, "author", id)
-    tages = TagList.getTagList()
-    author = Authors.getAuthorProfile(id)
+    try:
+        posts = PostList.getPostList(page, "author", id)
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page, "author", id)
+        tages = TagList.getTagList()
+        author = Authors.getAuthorProfile(id)
 
-    return render_to_response('author_post.html', locals())
+        return render_to_response('author_post.html', locals())
+    except:
+        sidePosts = PostList.getPostList(1)
+        page = PostList.getPageInfo(page)
+        tages = TagList.getTagList()
+        return render_to_response('404.html', locals())
